850a4fce1c90017dac170e4c09d5b88ccf2c3873
mvaivre <m.vaivre@gmail.com>
2014-03-13 18:54:44 +0100

    First config of the backend

--- velauz/__init__.py
+++ velauz/__init__.py
@@ -1,10 +1,7 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from .models import (
-    DBSession,
-    Base,
-    )
+from velauz.models import DBSession
 
 
 def main(global_config, **settings):
@@ -14,7 +11,11 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # Static config
     config.add_route('home', '/')
+    config.add_view('home', 'velauz:index.html', cache_max_age=3600) #velauz: est le package
+    
+
     config.scan()
     return config.make_wsgi_app()
