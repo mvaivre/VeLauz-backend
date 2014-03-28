0c8efcda781a85dedbd087dd41c4dedce03c93ae
mvaivre <m.vaivre@gmail.com>
2014-03-28 15:43:55 +0100

    Example of jsonp service

--- velauz/__init__.py
+++ velauz/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.renderers import JSONP
 from pyramid.mako_templating import renderer_factory as mako_renderer_factory
 
 
@@ -9,13 +10,14 @@ def main(global_config, **settings):
 
     config.add_renderer('.html', mako_renderer_factory)
     config.add_renderer('.js', mako_renderer_factory)
+    config.add_renderer('jsonp', JSONP(param_name='callback', indent=4))
 
     config.add_static_view('app', 'velauz:static/VeLauz-app')
 
     config.add_route('home', '/')
+    config.add_route('toto', '/service')
 
     config.add_view(route_name='home', renderer='velauz:templates/index.mako') #velauz: est le package
-
     # Static files
     config.scan()
     return config.make_wsgi_app()
