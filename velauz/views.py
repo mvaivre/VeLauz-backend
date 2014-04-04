43f904601ecf6fb4120bdf53f29fb2b947ad84bd
mvaivre <m.vaivre@gmail.com>
2014-04-04 17:22:15 +0200

    Modified views, login

--- velauz/views.py
+++ velauz/views.py
@@ -6,3 +6,21 @@ from pyramid.view import view_config
 def my_view(request):
     testParam = request.params.get('testParam')
     return { 'testme': testParam + 'yes' }
+
+@view_config(route_name='login', renderer='jsonp')
+def login(request):
+    if 'login' in request.params and 'password' in request.params:
+        userid = request.params['login']
+        password = request.params['password']
+        if check_password(login, password):
+            headers = remember(request, userid)
+            response = Response('')
+            response.headers.extend(headers)
+            return response
+        return HTTPUnauthorized(detail='Bad Login')
+    return HTTPUnauthorized(detail='Bad Login')
+
+
+def logout(request):
+    headers = forget(request)
+    return HTTPFound('/', headers=headers)
