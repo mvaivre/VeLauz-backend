cf4b0a3afda47de06c467ea403f0da9a608b030b
mvaivre <m.vaivre@gmail.com>
2014-04-07 18:57:39 +0200

    routes a bit modified

--- velauz/__init__.py
+++ velauz/__init__.py
@@ -12,14 +12,13 @@ def main(global_config, **settings):
     config.add_renderer('.js', mako_renderer_factory)
     config.add_renderer('jsonp', JSONP(param_name='callback', indent=4))
 
-    config.add_static_view('app', 'velauz:static/VeLauz-app')
+    config.add_static_view('/', 'velauz:static/VeLauz-app')
 
     #Routes 
     config.add_route('login', '/login')
-    config.add_route('home', '/')
     config.add_route('test', '/service')
 
-    config.add_view(route_name='home', renderer='velauz:templates/index.mako') #velauz: est le package
+    #config.add_view(route_name='home', renderer='velauz:templates/index.mako') #velauz: est le package
     # Static files
     config.scan()
     return config.make_wsgi_app()
